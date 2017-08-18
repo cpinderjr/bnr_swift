d7dd8bf4a7ae1a6444208fc66b03866028928aba
Carlos Pinder <carlos.pinder@icloud.com>
2017-08-18 15:43:13 -0400

    String chapter contents

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -2,4 +2,32 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+let playground = "Hello, playground"
+var mutablePlayground = "Hello, mutable playground"
+mutablePlayground += "!"
+
+for c: Character in mutablePlayground.characters {
+    print("'\(c)'")
+}
+
+let oneCoolDude = "\u{1F60E}"
+let aAcute = "\u{0061}\u{0301}"
+
+for scalar in playground.unicodeScalars {
+    print("\(scalar.value) ")
+}
+
+let aAcutePrecomposed = "\u{00E1}"
+
+let b = (aAcute == aAcutePrecomposed)
+
+print("aAcoute: \(aAcute.characters.count); aAcutePrecompose: \(aAcutePrecomposed.characters.count)")
+
+let start = playground.startIndex
+let end = playground.index(start, offsetBy: 4)
+let fifthCharacter = playground[end]
+
+let range = start...end
+let firstFive = playground[range]
+
+
